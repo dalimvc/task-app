ca832556e13f1dcfbd15bfa2b1c3b6d76b3b4f14
dalimvc <dalibor_mirkovic@live.com>
2025-08-07 17:03:51 +0200

    adding components

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Home Page</h2>
+            <p>Welcome to the Dojo Blog!</p>
+        </div>
+     );
+}
+ 
+export default Home;
